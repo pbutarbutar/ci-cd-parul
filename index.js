0a34047fe541d022e77e47a9d7a3093b8c8e37d2
Parulian B <parulian.butarbutar@pintek.id>
2020-10-20 12:19:02 +0700

    Fix test errored

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./server')
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 8080
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`This app running on http://localhost:${PORT}`)
 })
